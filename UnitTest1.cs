b67fec977e9b1f7f19b418e8a479cdd0b59c309c
Fernando Ibarra <luisfernandoibarrao@gmail.com>
2024-06-17 23:30:48 -0400

    Datos Vs

---
+++ UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace CI_CD_VS
+{
+    [TestClass]
+    public class PruebasUnitarias
+    {
+        [TestMethod]
+        public void TestIva()
+        {
+            Assert.AreEqual(8, 8);
+        }
+    }
+}
